5b89d0284712809de7e1f42ff39b2b631860206c
Tarek Aldali <187119989+Tarekaldali@users.noreply.github.com>
2026-04-28 02:30:06 +0300

    feat(ui): wrap App with PopupProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,20 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CartProvider } from "./contexts/CartContext";
+import { PopupProvider } from "./contexts/PopupContext";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <CartProvider>
+            <PopupProvider>
+              <App />
+            </PopupProvider>
+          </CartProvider>
+        </AuthProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
